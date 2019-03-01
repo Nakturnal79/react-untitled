14b80953a4e1d990c2416e84d3f2e2e122bd9af1
Nakturnal <nakturnal@rambler.ru>
2019-03-01 09:25:56 +0400

    extracting reusable form

--- src/components/registrationForm.jsx
+++ src/components/registrationForm.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Form from "./common/form";
 import Joi from "joi";
 
-class RegistrationForm extends Component {
+class RegistrationForm extends Form {
   state = {
     data: {
       username: "",
@@ -21,38 +22,10 @@ class RegistrationForm extends Component {
       .label("Password")
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
+  doSubmit = () => {
+    console.log("sumbitted");
   };
-  handleChange = e => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(e.currentTarget);
-    if (errorMessage) errors[e.currentTarget.name] = errorMessage;
-    else delete errors[e.currentTarget.name];
-    const data = { ...this.state.data };
-    data[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ data, errors });
-  };
-  validate = () => {
-    const result = Joi.validate(this.state.data, this.schema, {
-      abortEarly: false
-    });
-    if (!result.error) return null;
-    const errors = {};
-    for (let item of result.error.details) errors[item.path[0]] = item.message;
-    return errors;
 
-    // errors[result.]
-  };
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
   render() {
     return (
       <React.Fragment>
